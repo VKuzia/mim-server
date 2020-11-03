cf59aa1da5458cf395efda379b21b27abb953352
VKuzia <v.kuzia.mail@gmail.com>
2020-11-03 17:15:12 +0300

    Initial commit

---
+++ src/main/java/com/mimteam/mimserver/MimServerApplication.java
@@ -0,0 +1,11 @@
+package com.mimteam.mimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MimServerApplication.class, args);
+    }
+}

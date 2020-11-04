ca5fc4bab7185199df8c3b1883da0c1f9a570777
Max <mkogan58@gmail.com>
2020-11-04 18:07:08 +0300

    Add separate chat rooms
    Добавлены отдельные комнаты, сообщение теперь видно только в пределах той
    комнаты, где оно было отправлено.

--- src/main/java/com/mimteam/mimserver/controllers/ChatController.java
+++ src/main/java/com/mimteam/mimserver/controllers/ChatController.java
@@ -1,22 +1,35 @@
 package com.mimteam.mimserver.controllers;
 
 import com.mimteam.mimserver.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-    @MessageMapping("/chat.addUser")
-    @SendTo("/chat/public")
-    public ChatMessage handleAddUserMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    ChatController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
     }
 
-    @MessageMapping("/chat.message")
-    @SendTo("/chat/public")
-    public ChatMessage handleChatMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
+    @MessageMapping("/chats/{chatId}/addUser")
+    public void handleAddUserMessage(@DestinationVariable("chatId") String chatId,
+                                     @Payload ChatMessage chatMessage) {
+        sendMessageToChat(chatId, chatMessage);
+    }
+
+    @MessageMapping("/chats/{chatId}/sendMessage")
+    public void handleChatMessage(@DestinationVariable("chatId") String chatId,
+                                  @Payload ChatMessage chatMessage) {
+        sendMessageToChat(chatId, chatMessage);
+    }
+
+    private void sendMessageToChat(String chatId, ChatMessage message) {
+        simpMessagingTemplate.convertAndSend("/chats/" + chatId, message);
     }
 }

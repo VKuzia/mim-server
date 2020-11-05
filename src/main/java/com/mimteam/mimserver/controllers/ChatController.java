2c84369eb52ead96b0f725093385bbd21914f2ef
maxim12321 <31935266+maxim12321@users.noreply.github.com>
2020-11-05 11:09:20 +0300

    Configure base WebSocket (#1)
    Произведена начальная настройка WebSocket c логикой подписок
    на заданный URL, а именно:
    Добавлен класс WebSocketConfig, производящий начальную настройку WebSocket.
    Добавлен класс ChatController, который принимает сообщения от пользователя и
    рассылает его остальным.
    Добавлены отдельные комнаты, сообщение теперь видно только в пределах той
    комнаты, где оно было отправлено.
    Добавлен метод, принимающий запросы по удалению пользователя из конкретного чата.

---
+++ src/main/java/com/mimteam/mimserver/controllers/ChatController.java
@@ -0,0 +1,41 @@
+package com.mimteam.mimserver.controllers;
+
+import com.mimteam.mimserver.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    ChatController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @MessageMapping("/chats/{chatId}/addUser")
+    public void handleAddUserMessage(@DestinationVariable("chatId") String chatId,
+                                     @Payload ChatMessage chatMessage) {
+        sendMessageToChat(chatId, chatMessage);
+    }
+
+    @MessageMapping("/chats/{chatId}/removeUser")
+    public void handleRemoveUserMessage(@DestinationVariable("chatId") String chatId,
+                                        @Payload ChatMessage chatMessage) {
+        sendMessageToChat(chatId, chatMessage);
+    }
+
+    @MessageMapping("/chats/{chatId}/sendMessage")
+    public void handleChatMessage(@DestinationVariable("chatId") String chatId,
+                                  @Payload ChatMessage chatMessage) {
+        sendMessageToChat(chatId, chatMessage);
+    }
+
+    private void sendMessageToChat(String chatId, ChatMessage message) {
+        simpMessagingTemplate.convertAndSend("/chats/" + chatId, message);
+    }
+}

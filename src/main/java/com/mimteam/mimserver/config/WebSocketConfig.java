44a51c4eca20ff10ec4defffa88f03e1f83b7d9a
Max <mkogan58@gmail.com>
2020-11-05 10:44:14 +0300

    Remove unnecessary dependencies
    Убраны лишние зависимости в файле gradle.build и лишний импорт.

--- src/main/java/com/mimteam/mimserver/config/WebSocketConfig.java
+++ src/main/java/com/mimteam/mimserver/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.mimteam.mimserver.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;

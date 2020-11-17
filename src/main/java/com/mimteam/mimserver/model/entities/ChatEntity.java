7ade8b7f7859d986530efb2a6e7f19aa63a9addf
VKuzia <v.kuzia.mail@gmail.com>
2020-11-17 20:14:47 +0300

    Add ChatEntity and modify UserEntity
    Добавлен класс-запись для чата ChatEntity.
    Добавлены некоторые поля в UserEntity.

---
+++ src/main/java/com/mimteam/mimserver/model/entities/ChatEntity.java
@@ -0,0 +1,34 @@
+package com.mimteam.mimserver.model.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "mim", name = "chats")
+public class ChatEntity {
+
+    @Id
+    @GeneratedValue
+    private Integer chatId;
+
+    private String name;
+
+    public Integer getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Integer chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/mimteam/mimserver/model/entities/UserEntity.java
+++ src/main/java/com/mimteam/mimserver/model/entities/UserEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(schema = "mim", name = "users")
@@ -14,6 +15,9 @@ public class UserEntity {
     private Integer userId;
 
     private String name;
+    private String login;
+    private String passwordHash;
+    private LocalDateTime lastOnline;
 
     public Integer getUserId() {
         return userId;
@@ -29,5 +33,29 @@ public class UserEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getLastOnline() {
+        return lastOnline;
+    }
+
+    public void setLastOnline(LocalDateTime lastOnline) {
+        this.lastOnline = lastOnline;
     }
 }

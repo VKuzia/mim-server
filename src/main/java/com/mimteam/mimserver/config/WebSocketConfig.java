f7458ec952ad3716cd071b4ff84e134e1abf5756
Max <mkogan58@gmail.com>
2020-12-03 19:42:18 +0300

    Configure security for WebSockets
    Настроен Spring Security для вебсокетов. Поставлены ограничения на доступ
    только авторизованным пользователям.

--- src/main/java/com/mimteam/mimserver/config/WebSocketConfig.java
+++ src/main/java/com/mimteam/mimserver/config/WebSocketConfig.java
@@ -2,13 +2,14 @@ package com.mimteam.mimserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*");
@@ -18,5 +19,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/chat");
+    }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .nullDestMatcher().permitAll()
+                .simpDestMatchers("/app/**").authenticated()
+                .simpSubscribeDestMatchers("/chat/**").authenticated()
+                .anyMessage().denyAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
     }
 }

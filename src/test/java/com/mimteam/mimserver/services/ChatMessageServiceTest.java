0b00ebc79d2c943d4eec8e8bde54a8614c1309d8
Katerina <katealexposp@gmail.com>
2020-12-05 22:14:26 +0300

    Fix codestyle
    Удалены ненужные пробелы. Оптимизированы импорты

--- src/test/java/com/mimteam/mimserver/services/ChatMessageServiceTest.java
+++ src/test/java/com/mimteam/mimserver/services/ChatMessageServiceTest.java
@@ -10,7 +10,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
